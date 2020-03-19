2362250d1c37b90b49ecfb1522928c9943844546
Stephan <sgriesel@gmail.com>
2020-03-19 06:21:47 +0100

    add logic for total balance, income & expenses

--- script.js
+++ script.js
@@ -9,11 +9,12 @@ const amount = document.getElementById('amount');
 const testData = [
     { id: 1, text: 'Cheese', amount: -2 },
     { id: 2, text: 'Dog Walks', amount: 20 },
-    { id: 3, text: 'Kit-Kat', amount: -1 },
+    { id: 3, text: 'Kit-Kat', amount: -100 },
     { id: 4, text: 'Review', amount: 15 }
 ];
 
 let transactions = testData;
+console.log(transactions);
 
 // Add to DOM list
 function addTransactionDOM(transaction) {
@@ -25,6 +26,7 @@ function addTransactionDOM(transaction) {
     // Add class based on value
     item.classList.add(transaction.amount < 0 ? 'minus' : 'plus');
 
+    // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/abs
     item.innerHTML = `
         ${transaction.text} <span>${sign}${Math.abs(transaction.amount)}</span> 
         <button class="delete-btn">x</button>
@@ -33,11 +35,38 @@ function addTransactionDOM(transaction) {
     list.appendChild(item);
 }
 
+// Update the balance income and expense
+function updateValues() {
+    const amounts = transactions.map(transaction => transaction.amount);
+
+    const total = amounts.reduce((acc, item) => (acc += item), 0).toFixed(2);
+
+    console.log("total balance: " + total);
+
+    const income = amounts
+        .filter(item => item > 0)
+        .reduce((acc, item) => (acc += item), 0)
+        .toFixed(2);
+
+    console.log("income:" + income);
+
+    const expense = (amounts
+        .filter(item => item < 0)
+        .reduce((acc, item) => (acc += item), 0) * -1).toFixed(2);
+
+    console.log("outgoing expense:" + expense);
+
+    balance.innerText = `€${total}`;
+    money_plus.innerText = `€${income}`;
+    money_minus.innerText = `€${expense}`;
+}
+
 // Initialization
 function init() {
     list.innerHTML = '';
 
     transactions.forEach(addTransactionDOM);
+    updateValues();
 }
 
 init();
